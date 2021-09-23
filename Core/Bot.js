d57ac4327b825b4a363c97976d746c80c1cfb83d
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-09-23 20:43:50 +0200

    Working on core of discord bot

---
+++ Core/Bot.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const Discord = require("discord.js")
+const { DiscordAPI } = require('./DiscordAPI')
+
+const intents = new Discord.Intents(32767)
+
+module.exports = class Bot{
+    #bot = new Discord.Client({
+        intents
+    })
+
+    constructor() {
+        this.#bot.login(process.env.TOKEN)
+        
+        this.#bot.on('ready', () => {
+            console.log(`${this.#bot.user.username} is online!`)
+            this.#bot.user.setStatus("dnd")
+            this.#bot.user.setActivity("DataHunt Gate", ({type: 'WATCHING'}))
+            //this.#bot.user.setActivity("DataHunt Game", Discord.ActivityFlags.FLAGS.PLAY)
+            // DiscordAPI.Send(":white_check_mark:")
+            // DiscordAPI.Send(":x:")
+        })
+
+        this.#bot.on('messageCreate', message => {
+            if(message.content.startsWith(process.env.PREFIX)){
+                message.content = message.content.substr(1, message.content.length)
+                console.log(message.content)
+                let embed = new Discord.MessageEmbed()
+                .setColor('#00ff00')
+                .addFields(
+                    { name: 'User:', value: message.author.username},
+                    { name: 'Message:', value: message.content},
+                )
+                .setAuthor("DataHunt")
+                .setThumbnail(message.author.avatarURL({
+                    dynamic: true
+                }))
+                .setTimestamp()
+                .setFooter("DataHunt")
+                message.channel.send({embeds: [embed] })
+                switch (`${message.content}`) {
+                    case "pizza":
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+            }
+            
+        })
+    }
+}
