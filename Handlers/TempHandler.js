3fc59647da99530faee888030d0ada2677bbc4c5
EmileDeGamer <emilemolzoho@gmail.com>
2021-10-29 19:34:45 +0200

    able to show temp

--- Handlers/TempHandler.js
+++ Handlers/TempHandler.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
+const fs = require('fs')
 
 /**
  * 
  * @param {Discord.Client} bot 
  */
 module.exports = async function ShowTemp(bot){
-    const channelId = '903633080697380874'
+    const channelId = '903697884044730428'
     const channel = await bot.channels.fetch(channelId)
-    channel.setName('temp')
-    console.log(channel.name)
+    const temp = parseInt(fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8')) / 1000
+    channel.setName(`${temp} °C`)
 }
