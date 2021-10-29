074b365aa9e08621d7cce0cc3dac487b912065f9
EmileDeGamer <emilemolzoho@gmail.com>
2021-10-29 19:51:36 +0200

    fixed temp round bug

--- Handlers/TempHandler.js
+++ Handlers/TempHandler.js
@@ -8,6 +8,6 @@ const fs = require('fs')
 module.exports = async function ShowTemp(bot){
     const channelId = '903697884044730428'
     const channel = await bot.channels.fetch(channelId)
-    const temp = parseInt(fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8')) / 1000
+    const temp = Math.round(parseInt(fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8')) / 1000 * 10) / 10
     channel.setName(`${temp} °C`)
 }
